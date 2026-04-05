7e851e12d1ccbd791fb1e706042a994ead2d0101
NoumanElahi003 <noumanelahi19@gmail.com>
2026-04-05 22:57:11 +0500

    Player Movement + Idle,Walk Animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+
+    Rigidbody2D playerRB;
+    Animator playerAnim;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        playerRB.linearVelocity = new Vector2(horizontalInput, verticalInput) * 5f;
+
+        UpdateAnimations();
+        ChangeDirection();
+
+    }
+
+    void UpdateAnimations()
+    {
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            playerAnim.SetBool("isRunning", true);
+        }
+        else
+        {
+            playerAnim.SetBool("isRunning", false);
+        }
+    }
+
+    void ChangeDirection()
+    {
+        if(horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+        }
+        else if(horizontalInput > 0)
+        {
+            transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+        }
+    }
+}
